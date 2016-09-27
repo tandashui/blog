b4fc402b0397119470d751c5bb0885ccc0adf84f
tandashui <tandashui8686@163.com>
2016-09-27 14:07:05 +0800

    ceshi

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -7,7 +7,7 @@ return array(
 
     'MULTI_MODULE'          =>  true,
 	//'配置项'=>'配置值'
-    'URL_MODEL'             =>  '2',    //URL模式
+    'URL_MODEL'             =>  '3',    //URL模式
 	//数控信息
     'DB_TYPE'      =>  'mysql',                 // 数据库类型
     'DB_HOST'      =>  '120.27.188.126',        // 服务器地址
@@ -20,3 +20,15 @@ return array(
     'DB_CHARSET'   =>  'utf8mb4',                  // 数据库的编码 默认为utf8
     'SITE_URL'     =>  'http://www.tandashui.com',
 );
+
+location / {
+            #root   html;
+            index  index.html index.htm index.php;
+                 if (!-e $request_filename) {
+
+        rewrite  ^(.*)$  /index.php?s=$1  last;
+
+        break;
+
+    }
+}
