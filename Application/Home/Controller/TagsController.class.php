e15fe8e6b73c0ecaee03de45b70aad5631c785de
tandashui <tandashui8686@163.com>
2016-10-25 22:37:40 +0800

    chei

--- Application/Home/Controller/TagsController.class.php
+++ Application/Home/Controller/TagsController.class.php
@@ -4,8 +4,31 @@ use Think\Controller;
 class TagsController extends BaseController {
 
 	public function index(){
-		$this->assign("article_id",$article_id);
+
+		$term_model = M('terms');
+		$cats['parent']=array('neq',0);
+		$terminfo = $term_model->where($cats)->select();
+
+		// var_dump($terminfo);die;
+		$this->assign("terminfo",$terminfo);
         $this->display();
+	}
+
+	public function lists(){
+
+		$id=I('get.id');
+
+		 if(!empty($id)){
+              
+            $list=sp_sql_posts_paged("cid:$id;order:post_date DESC;",5);
+            // var_dump($list);die;
+        }else{
+                 $this->error('非法参数');
+        }
+
+		// var_dump($list);die;
+		$this->assign("list",$list);
+        $this->display("Index/index");
 	}
 
 }
